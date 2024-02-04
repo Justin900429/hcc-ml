b3ec8cc10d9808958e43e5c8468e91626003b595
Justin <justin900429@gmail.com>
2024-02-04 11:27:58 +0800

    update tello detection

--- new-lab3/tellosrc/receivers/detection.py
+++ new-lab3/tellosrc/receivers/detection.py
@@ -2,33 +2,25 @@ import json
 from threading import Lock
 
 import cv2
-import torch
 import numpy as np
 import requests
+import torch
 from PIL import Image
-from torchvision.transforms.functional import to_pil_image
-
-from yolov3.transforms import preprocess
-from yolov3.utils import draw_bbox, draw_text
-
 from tellosrc.base import ResourceThread
 from tellosrc.receivers.image import ImageReceiver
+from torchvision.transforms.functional import to_pil_image
+from utils.plots import plot_one_box
 
 
 class DetectionReceiver(ResourceThread):
-    def __init__(self,
-                 image_receiver: ImageReceiver,
-                 img_size,
-                 conf_threshold,
-                 nms_threshold,
-                 url):
+    def __init__(self, image_receiver: ImageReceiver, img_size, conf_threshold, nms_threshold, url):
         super().__init__()
         self.image_receiver = image_receiver
         self.img_size = img_size
         self.conf_threshold = conf_threshold
         self.nms_threshold = nms_threshold
         self.url = url
-        self.headers = {'content-type': 'image/jpeg'}
+        self.headers = {"content-type": "image/jpeg"}
 
         self.result_lock = Lock()
         self.id = None
@@ -44,39 +36,30 @@ class DetectionReceiver(ResourceThread):
             if id is None:
                 continue
 
-            raw = cv2.cvtColor(raw, cv2.COLOR_BGR2RGB)  # BGR to RGB
-            raw = Image.fromarray(raw).convert('RGB')   # convert to Pillow Image
-            raw_size = torch.tensor([raw.size[1], raw.size[0]]).long()
-            preprocess.update_img_size(self.img_size)
-            img, _ = preprocess(raw)
-            img = np.asarray(to_pil_image(img))         # convert to cv2 image
-            img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)  # RGB to BGR
-
-            _, img_encoded = cv2.imencode('.jpg', img)  # encode image
+            _, img_encoded = cv2.imencode(".jpg", raw)  # encode image
             params = {
-                'img_size': self.img_size,
-                'conf_threshold': self.conf_threshold,
-                'nms_threshold': self.nms_threshold,
+                "img_size": self.img_size,
+                "conf_threshold": self.conf_threshold,
+                "nms_threshold": self.nms_threshold,
             }
-            r = requests.post(                          # send request
-                self.url,
-                data=img_encoded.tobytes(),
-                headers=self.headers,
-                params=params)
-            r = json.loads(r.text)                      # decode response
-            bboxes = torch.tensor([pred['bbox'] for pred in r['predictions']])
-            scores = torch.tensor([pred['score'] for pred in r['predictions']])
-            labels = torch.tensor([pred['label'] for pred in r['predictions']])
-            colors = torch.tensor([pred['color'] for pred in r['predictions']])
-            names = [pred['name'] for pred in r['predictions']]
-            if len(bboxes) > 0:
-                bboxes = preprocess.revert(bboxes, raw_size, self.img_size)
-
+            r = requests.post(  # send request
+                self.url, data=img_encoded.tobytes(), headers=self.headers, params=params
+            )
+            r = json.loads(r.text)  # decode response
+            bboxes = torch.tensor([pred["bbox"] for pred in r["predictions"]])
+            scores = torch.tensor([pred["score"] for pred in r["predictions"]])
+            labels = torch.tensor([pred["label"] for pred in r["predictions"]])
+            colors = torch.tensor([pred["color"] for pred in r["predictions"]])
+            names = [pred["name"] for pred in r["predictions"]]
             # draw detections
             for bbox, score, color, name in zip(bboxes, scores, colors, names):
-                draw_bbox(raw, bbox, name, color)
-                draw_text(raw, bbox, name, color)
-            raw = cv2.cvtColor(np.asarray(raw), cv2.COLOR_RGB2BGR)
+                plot_one_box(
+                    bbox,
+                    raw,
+                    label=name,
+                    color=color.tolist(),
+                    line_thickness=1,
+                )
 
             # save detections and image
             with self.result_lock:
@@ -90,9 +73,7 @@ class DetectionReceiver(ResourceThread):
     def get_result(self):
         with self.result_lock:
             if self.id is None:
-                return self.id, (
-                    None, None, None, None, None
-                )
+                return self.id, (None, None, None, None, None)
             else:
                 return self.id, (
                     np.copy(self.img),
